bbb4d8bc692eea2a370360384c56fd0a119b24e4
yunxiu.wu <wu_yunxiu@nec.cn>
2019-05-22 18:57:33 +0800

    头部和底部组件，以及配置

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Header title="喵喵影院"></Header>
+        <TabBar></TabBar>
+    </div>
+</template>
+
+<script>
+    import Header from "../../components/Header/index";
+    import TabBar from "../../components/TabBar/index";
+    export default {
+        name:'cinema',
+        components: {TabBar, Header}
+    }
+</script>
+
+<style scoped>
+
+</style>
