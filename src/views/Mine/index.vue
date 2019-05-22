ccd33eda2443ab2dad6c3629d34849b19cc909b4
yunxiu.wu <wu_yunxiu@nec.cn>
2019-05-22 17:58:01 +0800

    头部和底部组件，以及配置

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="main">
+        <Header title="我的喵喵"></Header>
+        <TabBar></TabBar>
+    </div>
+</template>
+
+<script>
+    import Header from "../../components/Header/index";
+    import TabBar from "../../components/TabBar/index";
+    export default {
+        name: 'mine',
+        components: {TabBar, Header},
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Movie/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="main">
+        <Header title="喵喵电影"></Header>
+        <TabBar></TabBar>
+    </div>
+
+</template>
+
+<script>
+
+    import Header from "../../components/Header/index";
+    import TabBar from "../../components/TabBar/index";
+    export default {
+        name:'Movie',
+        components: {TabBar, Header},
+
+    }
+</script>
+
+<style scoped>
+
+</style>
